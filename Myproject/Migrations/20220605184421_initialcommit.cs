6a23bd903d7ec117ff9d0898a70185897f8fd9dd
Priyankjoshi-github <102507819+priyankjoshi-github@users.noreply.github.com>
2022-06-08 00:02:13 +0530

    Add project files.

---
+++ Myproject/Migrations/20220605184421_initialcommit.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Myproject.Migrations
+{
+    public partial class initialcommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    emailId = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    password = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarDetails",
+                columns: table => new
+                {
+                    emailId = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    contactNumber = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    carModel = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    carType = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    serviceType = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    preferredTime = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    message = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    address = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    subscription = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__CarDetai__87355E7297B3B789", x => x.emailId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carservice_Feedback",
+                columns: table => new
+                {
+                    feedback_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    First_Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Rating_Value = table.Column<decimal>(type: "decimal(10,0)", nullable: false),
+                    feedback_comments = table.Column<string>(type: "varchar(200)", unicode: false, maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Carservi__7A6B2B8CCE4E6D06", x => x.feedback_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    emailId = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    password = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Customer__87355E72420DD002", x => x.emailId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceLogin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    emailId = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    password = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceLogin", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+
+            migrationBuilder.DropTable(
+                name: "CarDetails");
+
+            migrationBuilder.DropTable(
+                name: "Carservice_Feedback");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "ServiceLogin");
+        }
+    }
+}
